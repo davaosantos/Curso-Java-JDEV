6a9c26056059bd68ea4ad0acbca81a132040d845
= <=>
2022-08-06 23:15:03 -0300

    atualizado até 13.15

--- src/cursojava/executavel/SplitArray.java
+++ src/cursojava/executavel/SplitArray.java
@@ -1,5 +1,8 @@
 package cursojava.executavel;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SplitArray {
 
     public static void main(String[] args) {
@@ -15,6 +18,23 @@ public class SplitArray {
         System.out.println("Curso: " + valoresArray[1]);
         System.out.println("Nota 1: " + valoresArray[2]);
         System.out.println("Nota 2: " + valoresArray[3]);
+
+
+        /*Convertendo um array em uma lista*/
+        List<String> list = Arrays.asList(valoresArray);
+        for (String valorString : list) {
+            System.out.println(valorString);
+        }
+
+        System.out.println("Nova conversao");
+
+        /*Convert de lista para array*/
+        String[] conversaoArray = list.toArray(new String[4]);
+        for (int pos = 0; pos < conversaoArray.length; pos++){
+            System.out.println(conversaoArray[pos]);
+        }
+
     }
+
 
 }
