09d3287fb7000bef3ac5f48989fd3ebf52b30bb7
= <=>
2022-08-14 18:18:36 -0300

    projeto corrigido e feito comentarios na ArrayVetor

--- src/cursojava/executavel/ArrayVetor.java
+++ src/cursojava/executavel/ArrayVetor.java
@@ -27,7 +27,7 @@ public class ArrayVetor {
         aluno.getDisciplinas().add(disciplina);
 
         Disciplina disciplina2 = new Disciplina();
-        disciplina.setDisciplina("Lógica");
+        disciplina2.setDisciplina("Lógica");
         disciplina2.setNota(notasPortugues);
 
         aluno.getDisciplinas().add(disciplina2);
@@ -43,41 +43,44 @@ public class ArrayVetor {
 
         /*Criação do aluno*/
         Aluno aluno2 = new Aluno();
-        aluno.setNome("David");
-        aluno.setNomeEscola("SHINGEKI curso");
+        aluno2.setNome("David");
+        aluno2.setNomeEscola("SHINGEKI curso");
 
         /*Criação da disciplina*/
         Disciplina disciplinaB = new Disciplina();
-        disciplina.setDisciplina("Curso de java");
-        disciplina.setNota(notas);
+        disciplinaB.setDisciplina("Curso de java");
+        disciplinaB.setNota(notas);
 
-        aluno.getDisciplinas().add(disciplinaB);
+        aluno2.getDisciplinas().add(disciplinaB);
 
         Disciplina disciplinaB2 = new Disciplina();
-        disciplina.setDisciplina("Lógica");
-        disciplina2.setNota(notasPortugues);
+        disciplinaB2.setDisciplina("Lógica");
+        disciplinaB2.setNota(notasPortugues);
 
-        aluno.getDisciplinas().add(disciplinaB2);
+        aluno2.getDisciplinas().add(disciplinaB2);
 
         Disciplina disciplinaB3 = new Disciplina();
-        disciplina3.setDisciplina("Culinaria");
-        disciplina3.setNota(notasCulinaria);
+        disciplinaB3.setDisciplina("Culinaria");
+        disciplinaB3.setNota(notasCulinaria);
 
-        aluno.getDisciplinas().add(disciplinaB3);
+        aluno2.getDisciplinas().add(disciplinaB3);
 
         //------------------------------------------
 
-        Aluno[] arrayAluno = new Aluno[1]; // cria um array de aluno , com tamanho 1
+        Aluno[] arrayAluno = new Aluno[2]; // cria um array de aluno , com tamanho 1
 
         arrayAluno[0] = aluno; // recebe o objeto aluno
         arrayAluno[1] = aluno2; // recebe o objeto aluno
 
+        //Itera os alunos
         for (int pos = 0; pos < arrayAluno.length; pos++){
-            System.out.println("Nome do aluno é: " + arrayAluno[pos].getNome());
+            System.out.println("\nNome do aluno é: " + arrayAluno[pos].getNome() + "\n");
 
+            //Itera as disciplinas do aluno
             for (Disciplina disc: arrayAluno[pos].getDisciplinas()) {
                 System.out.println("Nome da disciplina é : " + disc.getDisciplina());
 
+                //Itera as notas da disciplina
                 for(int posNota = 0 ; posNota < disc.getNota().length; posNota++){
                     System.out.println("A nota numero " + (posNota+1) + " foi : " + disc.getNota()[posNota]);
                 }
